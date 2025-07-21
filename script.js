c51820d70b27e3ca27bd564d659be775b7417bf7
Prakash Raut <prakash_raut@outlook.com>
2025-07-21 12:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let totalCost = 0;
+
+	while (arr.length  > 1) {
+		arr.sort((a,b) => a - b);
+		arr.splice(0, 2);
+		const cost = arr[0] + arr[1];
+		totalCost += cost;
+		arr.push(cost;)
+	}
+	return totalCost;
 }
 
 module.exports=mincost;
